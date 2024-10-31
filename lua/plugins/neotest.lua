f477b6298fa316892d9ee4ffa8fca3d164b9a99a
Alex Hamilton <adhamilt@ualberta.ca>
2024-10-30 19:08:05 -0600

    Trying to set up Rust and NeoTest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -9,7 +9,7 @@ return {
       -- or a table of adapter names, mapped to adapter configs.
       -- The adapter will then be automatically loaded with the config.
       adapters = {
-        ["neotest-rust"] = {},
+        ["rustaceanvim.neotest"] = {},
       },
       -- Example for loading neotest-golang with a custom config
       -- adapters = {
