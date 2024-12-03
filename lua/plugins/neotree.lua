6da15632865a9e627d9b277b21d788761d1e209e
Alex Hamilton <axhamilt@gmail.com>
2024-12-03 13:35:04 -0700

    open Terminal and Telescope in Neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,3 +1,25 @@
+local function getTelescopeOpts(state, path)
+  return {
+    cwd = path,
+    search_dirs = { path },
+    attach_mappings = function(prompt_bufnr, map)
+      local actions = require("telescope.actions")
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local action_state = require("telescope.actions.state")
+        local selection = action_state.get_selected_entry()
+        local filename = selection.filename
+        if filename == nil then
+          filename = selection[1]
+        end
+        -- any way to open the file without triggering auto-close event of neo-tree?
+        require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
+      end)
+      return true
+    end,
+  }
+end
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   cmd = "Neotree",
@@ -7,7 +29,8 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    "akinsho/toggleterm.nvim",
+    "nvim-telescope/telescope.nvim",
   },
   keys = {
     {
@@ -43,6 +66,28 @@ return {
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
+    },
+    window = {
+      mappings = {
+        ["tf"] = "telescope_find",
+        ["tg"] = "telescope_grep",
+        ["T"] = "toggle_terminal",
+      },
+    },
+    command = {
+      telescope_find = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        require("telescope.builtin").find_files(getTelescopeOpts(state, path))
+      end,
+      telescope_grep = function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        require("telescope.builtin").live_grep(getTelescopeOpts(state, path))
+      end,
+      toggle_terminal = function(state)
+        vim.cmd("ToggleTerm")
+      end,
     },
   },
 }
